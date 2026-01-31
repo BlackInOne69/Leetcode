385f1d2d169d1a6ca56b222ee2c290c3aa5a8faf
Pawan Poudel Sharma <80450406+BlackInOne69@users.noreply.github.com>
2026-01-31 20:20:05 +0545

    File updated at 1/31/2026, 8:20:05 PM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,16 +1,9 @@
-# Last updated: 1/31/2026, 8:13:40 PM
+# Last updated: 1/31/2026, 8:20:05 PM
 1class Solution:
-2    def isAnagram(self, s: str, t: str) -> bool: # Added 'self'
-3        if len(s) != len(t):
-4            return False
-5        
-6        count = {}
-7        for char in s:
-8            count[char] = count.get(char, 0) + 1
-9        
-10        for char in t:
-11            if char not in count or count[char] == 0:
-12                return False
-13            count[char] -= 1
-14            
-15        return True
+2    def moveZeroes(self, nums: list[int]) -> None:
+3        slow = 0 
+4        
+5        for fast in range(len(nums)):
+6            if nums[fast] != 0:
+7                nums[slow], nums[fast] = nums[fast], nums[slow]
+8                slow += 1
