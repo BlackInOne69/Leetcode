f7786917a60e26687207fdf9cb91067fc2c2d346
Pawan Poudel Sharma <80450406+BlackInOne69@users.noreply.github.com>
2026-07-08 21:26:47 +0545

    Create 1664-FindUsersWithValidEMails

---
+++ 1664-FindUsersWithValidEMails/1664-FindUsersWithValidEMails.sql
@@ -0,0 +1,11 @@
+-- Last updated: 08/07/2026, 21:26:47
+# Write your MySQL query statement below
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+    AND mail LIKE BINARY '%@leetcode.com';
