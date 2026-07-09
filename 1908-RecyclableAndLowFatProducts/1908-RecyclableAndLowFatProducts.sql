bee3a2c8bd461dfafc08508ba1365e9fc2e33bd6
Pawan Poudel Sharma <80450406+BlackInOne69@users.noreply.github.com>
2026-07-09 19:45:37 +0545

    Create 1908-RecyclableAndLowFatProducts

---
+++ 1908-RecyclableAndLowFatProducts/1908-RecyclableAndLowFatProducts.sql
@@ -0,0 +1,6 @@
+-- Last updated: 09/07/2026, 19:45:37
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT product_id
+FROM Products
+WHERE low_fats = "Y" AND recyclable = "Y";
