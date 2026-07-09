acb6ec1ea1632a661c555dd84adb436aa84c487b
Pawan Poudel Sharma <80450406+BlackInOne69@users.noreply.github.com>
2026-07-09 19:45:28 +0545

    Create 2110-EmployeesWithMissingInformation

---
+++ 2110-EmployeesWithMissingInformation/2110-EmployeesWithMissingInformation.sql
@@ -0,0 +1,10 @@
+-- Last updated: 09/07/2026, 19:45:28
+# Write your MySQL query statement below
+SELECT T.employee_id
+FROM  
+  (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+   UNION 
+   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+AS T
+WHERE T.salary IS NULL OR T.name IS NULL
+ORDER BY employee_id;
